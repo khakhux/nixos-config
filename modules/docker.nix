4575dacd697dac9733c7c0f8b4f70f4117c01eb3
sgen0291 <crparedes@igae.hacienda.gob.es>
2026-06-04 06:41:58 +0200

    added docker-buildx and COMPOSE_BAKE = "true" for compose build optimizations

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,14 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Docker and Compose
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
   };
 
-  # is this required?
   environment.systemPackages = with pkgs; [
     docker-compose
+    docker-buildx
   ];
+
+  environment.variables = {
+    COMPOSE_BAKE = "true"; # docker compose build optimizations
+  };
 }
